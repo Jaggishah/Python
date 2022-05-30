34304e591857d4079f50f4954eff2a03b56038fd
Jaggi shah <jaggishah@gmail.com>
2022-05-30 13:28:59 -0700

    classes 2

--- classes.py
+++ classes.py
@@ -1,9 +1,13 @@
 class jaggi:
     def __init__(self,name):
         self.name=name
+    
+    def music(self):
+        print("Music Is Life")
 
 
 l = jaggi("shah")
 print(l.name)
+l.music()
 
       
