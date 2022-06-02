c31fa40ad6cd9a87928574337053fa6b352c0993
Jaggi shah <jaggishah@gmail.com>
2022-06-02 12:34:12 -0700

    runnerup number

---
+++ runner up.py
@@ -0,0 +1,17 @@
+# n = int(input("entr the number"))
+arr=[3,6,4,0]
+c=0
+b=[]
+arr.sort()
+for i in arr:
+    # print(i)
+    if i > c:
+            c=i
+            b.append(c)
+        
+           
+           
+           
+            
+            
+print(b[-2])
