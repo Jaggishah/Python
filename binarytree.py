2ef15797a1a5f633ddb3fe5b65e8ecbcd8fb3b41
Jaggi shah <jaggishah@gmail.com>
2022-07-08 23:47:08 -0700

    binart tree

---
+++ binarytree.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self,data):
+        self.left=None
+        self.right=None
+        self.data=data
+
+    def insert(self,data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data=data
+        
+    def printtree(self):
+            if self.left:
+                self.left.printtree()
+            print(self.data)
+            if self.right:
+                self.right.printtree()
+    def inorder(self,k):
+        res=[]
+        if k:
+            res.append(k.data)
+            res=res +  self.inorder(k.left)
+            
+            res =res + self.inorder(k.right)
+
+        return res
+
+k = Node(10)
+k.insert(4)
+k.insert(12)
+k.insert(7)
+k.insert(17)
+print(k.inorder(k))
+    
+        
