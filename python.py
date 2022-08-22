90230543774280adbf17f5f463e64cf1a6f254c9
Jaggi shah <jaggishah@gmail.com>
2022-08-21 17:51:00 -0700

    changes

--- python.py
+++ python.py
@@ -1,13 +1,141 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d=len(nums)
-        for i in range(d-1):
-            for j in range(i+1,d):
-                if nums[i]+nums[j]==target:
-                    return[i,j]
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         d=len(nums)
+#         for i in range(d-1):
+#             for j in range(i+1,d):
+#                 if nums[i]+nums[j]==target:
+#                     return[i,j]
                 
-                
-                    
-            
-                    
-                        
+# a =['t','e','r','a']
+# b=""
+# for i in a:
+#     b+=i
+
+
+# c=['s','h','a','h']
+# k=""
+# for i in c:
+#     k+=i
+# print(b+k)
+
+
+# a = "bhawandeep"
+# b = []
+# for i in a:
+#     b.append(i)
+# print(b)
+
+# a = int(input("enter the number"))
+# if (a %2)==0:
+#     print("number is odd")
+# else:
+#     print("number is even")
+
+# a =[82,29,2920,2929]
+# a.reverse()
+# print(a)
+# a = (22,23,22,33)
+# print(a.count(22))
+# m1 = int(input("enter the number 1:"))
+# m2 = int(input("enter the number 2:"))
+# m3 = int(input("enter the number 3:"))
+# m4 = int(input("enter the number 4:"))
+# number=[m1,m2,m3,m4]
+# number.sort()
+# print(number)
+
+# a =[2,3,5,5,]
+# a = (a[0]+a[1]+a[2]+a[3])
+# b = int(sum(a))
+# print(b)
+
+# b = []
+# k =int(input("enter the number of the list"))
+# for i in range(k):
+#     a = input("enter the number")
+#     b.append(a)
+# print(b)
+    
+# a = {18,"18",18.1}
+# print(a)
+
+# s = {20,"20",20.0}
+# # print(s)
+# print(len(s))
+
+# num1 = int(input("enter the number 1:"))
+# num2 = int(input("enter the number 2:"))
+# num3 = int(input("enter the number 3:"))
+# num4 = int(input("enter the number 4:"))
+# if (num1>num4):
+#     f1 = num1
+# else:
+#     f1 = num4
+# if (num2>num3):
+#     f2 = num2
+# else:
+#     f2 = num3
+# if (f1>f2):
+#     print(str(f1)+ "is the greater") 
+# else:
+#     print(str(f2)+ "is the greater")
+
+
+# a = "bhawandeep"
+# b=[]
+# for i in a:
+#     b.append(i)
+# print(b)
+# a= ['b','h','a','w','a','n','d','e','e','p']
+# c=""
+# for i in a:
+#     c+=i
+
+# a=['s','i','n','g','h']
+# d=""
+# for i in a:
+#     d+=i
+
+# a = ['s','h','a','h']
+# b=""
+# for i in a:
+#     b+=i
+
+
+# a = [1,3,34,43]
+# b=""
+# for i in a:
+#     b+=str(i)
+# print(b)
+
+# a = input("enter the number")
+# b = input("enter the letter")
+# c = str()
+# for i in a:
+#     if i == b:
+#        continue
+#     else:
+#         c+=i
+# print(c)
+
+# a = "bhawandeep shah"
+# b =[]
+# for i in a:
+#     b.append(i)
+# print(b)
+
+
+
+# a= [1,3,3,32,32,3,]
+# b=""
+# for i in a:
+#     b+=str(i)
+# print(b)
+
+# a= ['b','h','a','w','a','n']
+# b=""
+# for i in a:
+#     b+=i
+# print(b)
+
+
